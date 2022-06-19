0429ca543e677e4260a95812f9766eceb08ff2a3
Gerardo Rosiles <jose.g.rosiles@saic.com>
2022-06-18 18:12:42 -0600

    Code updates

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,10 +1,30 @@
 'use strict';
 
 // Data needed for a later exercise
-const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+//const flights =
+//  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
 // Data needed for first part of the section
+//ES6 enhanced object literals
+//computer property names instead of having to
+//write them manually or literally
+
+const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+const openingHours = {
+  [weekdays[3]]: {
+    open: 12,
+    close: 22,
+  },
+  [weekdays[4]]: {
+    open: 11,
+    close: 23,
+  },
+  [weekdays[5]]: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+
 const restaurant = {
   name: 'Classico Italiano',
   location: 'Via Angelo Tavanti 23, Firenze, Italy',
@@ -12,23 +32,15 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-  order: function (starterIndex, mainIndex) {
-    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
-  },
+  //ES6 enhanced object literals
+  openingHours,
 
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
+  //ES6 enhnaced object literal for methods
+  //no need to use the function reserved word
+  //just name property with params directly
+
+  order(starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
 
   //orderDelivery: function (obj) {
@@ -57,6 +69,460 @@ const restaurant = {
 };
 
 ///////////////////////////////////////
+// Coding Challenge #4
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+const getCode = str => str.slice(0, 3).toUpperCase();
+
+for (const flight of flights.split('+')) {
+  const [type, from, to, time] = flight.split(';');
+  let output = `${type.startsWith('_Delayed') ? '🔴' : ''}`;
+  output += type.replaceAll('_', ' ');
+  output += ` ${getCode(from)} to ${getCode(to)}}`;
+  output += `(${time.replace(':', 'h')})`;
+  output = output.padStart(40);
+  console.log(output);
+}
+
+/* 
+Write a program that receives a list of variable names written 
+in underscore_case and convert them to camelCase.
+
+The input will come from a textarea inserted into the DOM 
+(see code below), and conversion will happen when the button 
+is pressed.
+
+THIS TEST DATA (pasted to textarea)
+underscore_case
+ first_name
+Some_Variable 
+  calculate_AGE
+delayed_departure
+
+SHOULD PRODUCE THIS OUTPUT (5 separate console.log outputs)
+underscoreCase      ✅
+firstName           ✅✅
+someVariable        ✅✅✅
+calculateAge        ✅✅✅✅
+delayedDeparture    ✅✅✅✅✅
+
+HINT 1: Remember which character defines a new line in the textarea 😉
+HINT 2: The solution only needs to work for a variable made out of 2 words, like a_b
+HINT 3: Start without worrying about the ✅. Tackle that only after you have the variable name conversion working 😉
+HINT 4: This challenge is difficult on purpose, so start watching the solution in case you're stuck. Then pause and continue!
+
+Afterwards, test with your own test data!
+
+GOOD LUCK 😀
+*/
+
+/*
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+
+document.querySelector('button').addEventListener('click', function () {
+  const varString = document.querySelector('textarea').value;
+  converToCamelCase(varString);
+  document.querySelector('textarea').value = '';
+});
+
+function converToCamelCase(varString) {
+  const underScoreVars = varString.split('\n');
+
+  for (const [j, v] of underScoreVars.entries()) {
+    const splitVarArray = v.trim().toLowerCase().split('_');
+
+    let camelCaseVar = '';
+    for (const [i, s] of splitVarArray.entries()) {
+      if (i > 0) {
+        camelCaseVar += s[0].toUpperCase() + s.slice(1);
+      } else {
+        camelCaseVar = s;
+      }
+    }
+
+    console.log(camelCaseVar.padEnd(20) + '✅'.repeat(j + 1));
+  }
+}
+*/
+// const varString =
+//   'underscore_case \n first_name \n Some_Variable \n calculate_AGE \n delayed_departure ';
+
+// converToCamelCase(varString);
+
+/*
+/////////////////////////////////
+// Working with Strings - Part 3
+
+//Split and join
+console.log('a+very+nice+string'.split('+'));
+console.log('Jonas Schmedtmann'.split(' '));
+
+const [firstName, lastName] = 'Jonas Schmedtmann'.split(' ');
+
+const newName = ['Mr.', firstName, lastName.toUpperCase()].join(' ');
+console.log(newName);
+
+const capitalizeName = function (name) {
+  const names = name.split(' ');
+  const namesUpper = [];
+
+  for (const n of names) {
+    //namesUpper.push(n[0].toUpperCase() + n.slice(1));
+    namesUpper.push(n.replace(n[0], n[0].toUpperCase()));
+  }
+  console.log(namesUpper.join(' '));
+};
+
+capitalizeName('jessica ann smith davis');
+capitalizeName('jonas schmedtmann');
+capitalizeName('jose gerardo rosiles');
+
+// Padding
+const message = 'Go to gate 23!';
+console.log(message.padStart(20, '+').padEnd(30, '+'));
+console.log('Jonas'.padStart(20, '+').padEnd(30, '+'));
+
+const maskCreditCard = number => {
+  const str = number + '';
+  const last = str.slice(-4);
+  return last.padStart(str.length, '*');
+};
+
+console.log(maskCreditCard(64637836));
+console.log(maskCreditCard(43378463864647384));
+console.log(maskCreditCard('334859493847755774747'));
+
+// Repeat
+const message2 = 'Bad weather... All Departures Delayed... ';
+console.log(message2.repeat(5));
+
+const planesInLine = function (n) {
+  console.log(`There are ${n} planes in line ${'🛩️'.repeat(n)}`);
+};
+planesInLine(5);
+planesInLine(3);
+planesInLine(12);
+
+const planesInLineArrow = n => {
+  console.log(`There are ${n} planes in line ${'🛩️'.repeat(n)}`);
+};
+planesInLineArrow(4);
+planesInLineArrow(6);
+*/
+/*
+////////
+//Working with strings - Part 2
+const airline = 'TAP Air Portugal';
+
+console.log(airline.toLowerCase());
+console.log(airline.toUpperCase());
+
+// Fix capitalization in name
+const passenger = 'jOnAS'; //Jonas
+const passengerLower = passenger.toLowerCase();
+const passengerCorrect =
+  passengerLower[0].toUpperCase() + passengerLower.slice(1);
+console.log(passengerCorrect);
+
+//Comparing emails
+const email = 'hello@jonas.io';
+const loginEmail = '    Hello@Jonas.IO \n';
+
+//const lowerEmail = loginEmail.toLowerCase();
+//const trimmedEmail = lowerEmail.trim();
+//console.log(trimmedEmail);
+
+const normalizedEmail = loginEmail.toLowerCase().trim();
+console.log(normalizedEmail);
+console.log(email === normalizedEmail);
+
+//replacing
+const priceGB = '288.97£';
+const priceUS = priceGB.replace('£', '$').replace(',', '.');
+console.log(priceUS);
+
+const announcement =
+  'All passengers come to boarding door 23. Boarding door 23!';
+
+//console.log(announcement.replace('door', 'gate'));
+// console.log(announcement.replaceAll('door', 'gate'));
+console.log(announcement.replace(/door/g, 'gate'));
+
+// Booleans
+const plane = 'Airbus A320 neo';
+console.log(plane.includes('A320'));
+console.log(plane.includes('Boeing'));
+console.log(plane.startsWith('Airb'));
+
+if (plane.startsWith('Airbus') && plane.endsWith('neo')) {
+  console.log('Part of the NEW Airbus family');
+}
+
+//Practice exercise
+const checkBaggage = function (items) {
+  const baggage = items.toLowerCase();
+
+  if (baggage.includes('knife') || baggage.includes('gun')) {
+    console.log('You are NOT allowed on board');
+  } else {
+    console.log('Welcome aboard!');
+  }
+};
+
+const checkBaggageArrow = items => {
+  const baggage = items.toLowerCase();
+
+  if (baggage.includes('knife') || baggage.includes('gun')) {
+    console.log('You are NOT allowed on board');
+  } else {
+    console.log('Welcome aboard!');
+  }
+};
+
+checkBaggage('I have a laptop, some Food and a pocket Knife');
+checkBaggage('Socks and camera');
+checkBaggage('Got some snacks and a gun for protection');
+
+checkBaggageArrow('Shirt, Pants, SHOES');
+checkBaggageArrow('Knife, Gun, Fedora');
+*/
+/*
+/////////
+//Working with strings - Part 1
+const airline = 'TAP Air Portugal';
+const plane = 'A320';
+
+console.log(plane[0]);
+console.log(plane[1]);
+console.log(plane[2]);
+console.log('B737'[0]);
+
+console.log(airline.length);
+console.log('B733'.length);
+
+//string methods
+console.log(airline.indexOf('r'));
+console.log(airline.lastIndexOf('r'));
+console.log(airline.indexOf('Portugal'));
+
+//use slice method to extract parts of strings
+//e.g., substrings
+console.log(airline.slice(4)); //returns a new string
+console.log(airline.slice(4, 7)); //Using and end value. The end value is not extracted, only 4,5,6
+//length 7 - 4 = 3
+
+//using other methods to extract posiitonal indices
+console.log(airline.slice(0, airline.indexOf(' ')));
+console.log(airline.slice(airline.lastIndexOf(' ') + 1));
+
+console.log(airline.slice(-2));
+console.log(airline.slice(1, -1));
+
+//Practice
+const checkMiddleSeat = function (seat) {
+  // B and E are middle seats
+  const s = seat.slice(-1);
+  if (s == 'B' || s == 'E') {
+    console.log('You got the middle seat 😀');
+  } else {
+    console.log('You got lucky 😎');
+  }
+};
+
+checkMiddleSeat('11B');
+checkMiddleSeat('23C');
+checkMiddleSeat('3E');
+
+//Strings are primitives, JS behind the scene takes the string and converts it
+//to a string object, i.e., boxes the string primitive
+//see the methods for this String object in the console
+console.log(new String('jonas'));
+
+//when operation is done, the object is converted back to a
+//regular spring primitive ... All String method return a string
+//primitice
+console.log(typeof new String('jonas'));
+console.log(typeof new String('jonas').slice(1));
+*/
+///////////////////////////////////////
+// Coding Challenge #2
+
+/* 
+Let's continue with our football betting app!
+
+1. Loop over the game.scored array and print each player name to the console, along with the goal number (Example: "Goal 1: Lewandowski")
+2. Use a loop to calculate the average odd and log it to the console 
+   (We already studied how to calculate averages, you can go check if you don't remember)
+3. Print the 3 odds to the console, but in a nice formatted way, 
+   exaclty like this:
+      Odd of victory Bayern Munich: 1.33
+      Odd of draw: 3.25
+      Odd of victory Borrussia Dortmund: 6.5
+Get the team names directly from the game object, don't hardcode them (except for "draw"). HINT: Note how the odds and the game objects have the same property names 😉
+
+BONUS: Create an object called 'scorers' which contains the names of the players who scored as properties, and the number of goals as the value. In this game, it will look like this:
+      {
+        Gnarby: 1,
+        Hummels: 1,
+        Lewandowski: 2
+      }
+
+GOOD LUCK 😀
+*/
+/*
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+//Question 1
+for (const [index, scorer] of game.scored.entries()) {
+  console.log(`Goal ${index + 1} : ${scorer}`);
+}
+
+//Question 2
+//2. Use a loop to calculate the average odd and log it to the console
+//   (We already studied how to calculate averages, you can go check if you don't remember)
+const values = Object.values(game.odds);
+let sum = 0;
+for (const v of values) {
+  sum += v;
+}
+console.log(`odds average = ${sum / values.length}`);
+
+//Question 3
+// Example output format
+// Odd of victory Bayern Munich: 1.33
+// Odd of draw: 3.25
+// Odd of victory Borrussia Dortmund: 6.5
+const odds_keys = Object.keys(game.odds);
+for (const s of odds_keys) {
+  const favorite_result = game[s] ?? 'draw';
+  console.log(`Odd of victory ${favorite_result} : ${game.odds[s]}`);
+}
+console.log();
+
+//Question 4
+// {
+//   Gnarby: 1,
+//   Hummels: 1,
+//   Lewandowski: 2
+// }
+const scorers = {};
+
+for (const s of game.scored) {
+  scorers[s] ? scorers[s]++ : (scorers[s] = 1);
+}
+console.log(scorers);
+*/
+/*
+//Looping Objects, Objects key, values and entries
+// Property Names
+const properties = Object.keys(openingHours);
+console.log(properties);
+
+let openStr = `We are open ${properties.length} days on :`;
+
+for (const day of properties) {
+  openStr += ` ${day},`;
+}
+console.log(openStr);
+
+//Property Values
+const values = Object(openingHours);
+console.log(values);
+
+//Entire object
+const entries = Object.entries(openingHours);
+console.log(entries);
+
+for (const [day, { open: starting, close: ending }] of entries) {
+  console.log(`On ${day} we open at ${starting} and close at ${ending}`);
+}
+*/
+/*
+//conditional chaining
+//short circuiting with &&
+if (restaurant.openingHours && restaurant.openingHours.mon) {
+  console.log(restaurant.openingHours.mon.open);
+}
+console.log(restaurant.openingHours?.mon?.open);
+
+//WITH optional chaining
+console.log(restaurant.openingHours.mon?.open);
+
+//Example
+const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+for (const day of days) {
+  const open = restaurant.openingHours[day]?.open ?? 'closed';
+  console.log(`On ${day}, we are open at ${open}`);
+}
+//Methods
+console.log(restaurant.order?.(0, 1) ?? 'Method does not exist');
+console.log(restaurant.orderRissoto?.(0, 1) ?? 'Method does not exist');
+
+//Arrays
+const users = [{ name: 'Jonas', email: 'hello@jonas.io' }];
+console.log(users[0]?.name ?? 'User array empty');
+*/
+//
+//Enhanced object literals
+//see mods to restaurant object
+/*
+//for off
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+
+for (const item of menu) console.log(item);
+
+// for (const item of menu.entries()) {
+//   console.log(` ${item[0] + 1}: ${item[1]}`);
+// }
+
+//use destructuring
+for (const [i, el] of menu.entries()) {
+  console.log(` ${i + 1}: ${el}`);
+}
+*/
+
+///////////////////////////////////////
 // Coding Challenge #1
 
 /* 
@@ -76,6 +542,7 @@ TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Th
 
 GOOD LUCK 😀
 */
+/*
 const game = {
   team1: 'Bayern Munich',
   team2: 'Borrussia Dortmund',
@@ -156,6 +623,7 @@ printGoals(...game.scored);
 //Step 7
 (team1 < team2 && console.log('Team 1 more likely to win')) ||
   (team2 < team1 && console.log('Team 2 more likely to win'));
+*/
 /*
 //The spread operator
 const arr = [7, 8, 9];
@@ -208,7 +676,7 @@ console.log(ingredients);
 restaurant.orderPasta(ingredients[0], ingredients[1], ingredients[2]);
 restaurant.orderPasta(...ingredients);
 
-//Since ES18, you can also use the spread operator with Objects
+//Since ES16, you can also use the spread operator with Objects
 const newRestaurant = { foundedIn: 1998, ...restaurant, founder: 'Giussepe' };
 console.log(newRestaurant);
 
--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,62 @@
 'use strict';
+
+// const bookings = [];
+
+// const createBooking = function (
+//   flightNum,
+//   numPassengers = 1,
+//   price = 199 * numPassengers
+// ) {
+//   //Use advanced ES6 literals, declare defuaults in function's signature
+
+//   //ES5 old way
+//   // numPassengers = numPassengers || 1;
+//   // price = price || 199;
+
+//   const booking = {
+//     flightNum,
+//     numPassengers,
+//     price,
+//   };
+//   console.log(booking);
+//   bookings.push(booking);
+// };
+// console.log('Hello');
+// createBooking('LMH123');
+// createBooking('LMH123', 2, 800);
+// createBooking('LM1234', 3);
+// createBooking('LM1234', 5);
+// //Note: you can't skip arguments. Need to follow order in which
+// //they are defined
+// //A trick to skip params and used their default value is to use
+// //undefined
+// createBooking('LMJ1234', undefined, 350);
+
+// const x = -1;
+// const y = x ?? 1;
+// console.log(y);
+
+const flight = 'LH234';
+const gerardo = {
+  name: 'Gerardo Rosiles',
+  passport: 12334545,
+};
+
+//primitive types are passed by value, a copy of the value passed
+//if placed in the paramereter
+//objet types are passed by reference. Objets live in the heap
+//any change to object inside the function are preserved after
+//function is done
+const checking = function (flightNumber, passenger) {
+  flightNumber = 'LH999';
+  passenger.name = 'Mr. ' + passenger.name;
+  if (passenger.passport == 12334545) {
+    alert('Checked in');
+  } else {
+    alert('Wrong passport!');
+  }
+  console.log(flightNumber, passenger);
+};
+
+checking(flight, gerardo);
+console.log(flight, gerardo);
